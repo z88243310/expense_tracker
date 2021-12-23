185685bf7bd7381b775365c0d0a932ce25ed3853
z88243310 <z88243310@gmail.com>
2021-12-23 18:14:46 +0800

    project init

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
+
+// set env if NODE_ENV isn't production
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const PORT = process.env.PORT
+const routes = require('./routes')
+
+// set handlebars
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+// use body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// 請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
+
+// entry of router
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)git 
+})
