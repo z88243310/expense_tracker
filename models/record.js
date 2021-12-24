f9139484ddbc041232138d28e892bd5cf06d2395
z88243310 <z88243310@gmail.com>
2021-12-24 15:10:30 +0800

    feat: add user & record & category models

---
+++ models/record.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true,
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('Record', recordSchema)
