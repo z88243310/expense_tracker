7449e8c402a055ce9b6e837fefc6d6cb13916da5
z88243310 <z88243310@gmail.com>
2021-12-27 18:30:42 +0800

    feat: add search function

--- public/javascripts/index-page.js
+++ public/javascripts/index-page.js
@@ -1,7 +1,17 @@
 const categorySelect = document.querySelector('#category-select')
+const searchInput = document.querySelector('#search-input')
 
+// 當 categorySelect 改變時，發送 request categoryIdSelected & keyword
 categorySelect.addEventListener('change', function onCategorySelectChanged(event) {
   const target = event.target
   const categoryIdSelected = target.value
-  location.replace(`/?categoryIdSelected=${categoryIdSelected}`)
+  const keyword = searchInput.value
+  location.replace(`/?categoryIdSelected=${categoryIdSelected}&keyword=${keyword}`)
 })
+
+// // 去除 特殊符號
+// searchInput.addEventListener('keyup', function onSearchInputKeyUP(event) {
+//   const target = event.target
+//   target.value = target.value.replace(/[^\a-\z\A-\Z0-9\u4E00-\u9FA5]/g, '')
+//   // target.value = target.value.replace(/[^\a-\z\A-\Z0-9\u4E00-\u9FA5]/g, '')
+// })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,13 +7,15 @@ const getFormatDate = require('../../tools/getFormatDate')
 const getTotalAmount = require('../../tools/getTotalAmount')
 
 router.get('/', async (req, res) => {
-  const categoryIdSelected = req.query.categoryIdSelected
   const userId = req.user._id
+  // search keyword & categoryId selected
+  const keyword = req.query.keyword || ''
+  const categoryIdSelected = req.query.categoryIdSelected || ''
   try {
     // 如果 categoryIdSelected 為 有效值，則 列入篩選條件
     const categories = await Category.find().lean()
     const categoryIndex = categories.findIndex(category => JSON.stringify(category._id) === JSON.stringify(categoryIdSelected))
-    const records = await Record.find(categoryIndex === -1 ? { userId } : { userId, categoryId: categoryIdSelected })
+    const records = await Record.find(categoryIndex === -1 ? { userId, name: { $regex: keyword } } : { userId, categoryId: categoryIdSelected, name: { $regex: keyword } })
       .populate('categoryId')
       .sort({ date: 'desc' })
       .lean()
@@ -21,7 +23,7 @@ router.get('/', async (req, res) => {
     // 調整時間格式 > 計算總金額 > 渲染畫面
     getFormatDate(records)
     const totalAmount = getTotalAmount(records)
-    return res.render('index', { records, categories, totalAmount, categoryIdSelected })
+    return res.render('index', { records, categories, totalAmount, categoryIdSelected, keyword })
   } catch (error) {
     return console.log(error)
   }
