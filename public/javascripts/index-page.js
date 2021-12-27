660644e3382770280f697a6386bac5c87440526f
z88243310 <z88243310@gmail.com>
2021-12-28 00:46:11 +0800

    feat:add delete listener to confirm again

--- public/javascripts/index-page.js
+++ public/javascripts/index-page.js
@@ -1,5 +1,6 @@
 const categorySelect = document.querySelector('#category-select')
 const searchInput = document.querySelector('#search-input')
+const deleteButtons = document.querySelectorAll('#delete-btn')
 
 // 當 categorySelect 改變時，發送 request categoryIdSelected & keyword
 categorySelect.addEventListener('change', function onCategorySelectChanged(event) {
@@ -13,4 +14,13 @@ categorySelect.addEventListener('change', function onCategorySelectChanged(event
 searchInput.addEventListener('keyup', function onSearchInputKeyUP(event) {
   const target = event.target
   target.value = target.value.replace(/[\(\)]|[\[\]]|[\\\?\+\*]/g, '')
+})
+
+// 刪除再次確認
+deleteButtons.forEach(deleteButton => {
+  deleteButton.addEventListener('click', function onDeleteButtonClicked(event) {
+    const target = event.target
+    event.preventDefault()
+    if (confirm('確定刪除嗎?')) target.parentElement.submit()
+  })
 })
