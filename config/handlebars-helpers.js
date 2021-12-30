707257fe0e5eedb0c97941acc9d7a5999fa9980b
z88243310 <z88243310@gmail.com>
2021-12-30 19:58:53 +0800

    refator: beautify list-item and button on index page

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -11,5 +11,12 @@ module.exports = {
       count += options.fn(i)
     }
     return count
+  },
+  ifOdd: function (conditional, options) {
+    if (conditional % 2 == 0) {
+      return options.fn(this)
+    } else {
+      return options.inverse(this)
+    }
   }
 }
