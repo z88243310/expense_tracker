3e1b58070fcdb55189b5088a5feb2d89944cbae1
z88243310 <z88243310@gmail.com>
2022-01-12 14:56:27 +0800

    refactor: clear unnecessary code on getFormatDate.js

--- tools/getFormatDate.js
+++ tools/getFormatDate.js
@@ -1,11 +1,7 @@
 const moment = require('moment')
 
 module.exports = records => {
-  if (!records.date) {
-    records.map(record => {
-      record.date = moment(record.date).format('YYYY-MM-DD')
-    })
-  } else {
-    records.date = moment(records.date).format('YYYY-MM-DD')
-  }
+  records.forEach(record => {
+    record.date = moment(record.date).format('YYYY-MM-DD')
+  })
 }
